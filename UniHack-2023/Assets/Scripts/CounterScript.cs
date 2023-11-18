117a8efe78f570f59fcc549f8041696028e00f17
Anleglift <126957403+Anleglift@users.noreply.github.com>
2023-11-18 21:00:00 +0200

    Finished_All_UI

---
+++ UniHack-2023/Assets/Scripts/CounterScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CounterScript : MonoBehaviour
+{
+    public int sum=0;
+    public TextMeshProUGUI text;
+    void Start()
+    {
+        // Start invoking the GenerateRandomNumber method every 5 seconds.
+        InvokeRepeating("GenerateRandomNumber", 0f, 5f);
+    }
+
+    void GenerateRandomNumber()
+    {
+        // Generate a random number between 1 and 10 (inclusive).
+        int randomNumber = Random.Range(1, 11);
+        sum = sum + randomNumber;
+        text.text = sum.ToString() + "items";
+    }
+}
